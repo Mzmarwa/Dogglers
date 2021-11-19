45007222ce00c72a733c6df24ecfb4060682147e
Marwa Ziadia <marwa.ziadia1@gmail.com>
2021-11-20 00:04:29 +0100

    Commit

---
+++ app/src/main/java/com/marwa/dogglers/VerticalListActivity.kt
@@ -0,0 +1,29 @@
+package com.marwa.dogglers
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.marwa.dogglers.adapter.DogCardAdapter
+import com.marwa.dogglers.const.Layout
+import com.marwa.dogglers.databinding.ActivityVerticalListBinding
+
+class VerticalListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityVerticalListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityVerticalListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.verticalRecyclerView.adapter = DogCardAdapter(
+            applicationContext,
+            Layout.VERTICAL
+        )
+
+        // Specify fixed size to improve performance
+        binding.verticalRecyclerView.setHasFixedSize(true)
+
+        // Enable up button for backward navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
